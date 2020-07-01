71a208621615b984bf61f98d2ee8a49b70a6ca3a
bhomass <bhomass1@cox.net>
2020-06-30 19:10:32 -0700

    print out cwd

--- legal_corp/legal_agent.py
+++ legal_corp/legal_agent.py
@@ -1,11 +1,12 @@
 from allennlp.predictors.predictor import Predictor as AllenNLPPredictor
-
+import os
 
 class Legal_agent:
     def __init__(self):
         self.predictor = AllenNLPPredictor.from_path(
             "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2020.03.19.tar.gz"
         )
+        print('cwd={}'.format(os.getcwd()))
         with open('legal_corp/data/1000453_1997-11-14_CREDIT AGREEMENT.txt', 'r') as credit_doc:
             content = credit_doc.read()
             
