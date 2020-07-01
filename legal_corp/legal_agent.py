825741bab1dfc8252ec763f1720b6650bab48291
bhomass <bhomass1@cox.net>
2020-06-30 19:40:44 -0700

    fix filepath

--- legal_corp/legal_agent.py
+++ legal_corp/legal_agent.py
@@ -6,14 +6,16 @@ class Legal_agent:
         self.predictor = AllenNLPPredictor.from_path(
             "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2020.03.19.tar.gz"
         )
-        print('cwd={}'.format(os.getcwd()))
-        with open('legal_corp/data/1000453_1997-11-14_CREDIT AGREEMENT.txt', 'r') as credit_doc:
+        filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'data', '1000453_1997-11-14_CREDIT AGREEMENT.txt')
+        print('cwd={}'.format(filepath))
+        with open(filepath, 'r') as credit_doc:
             content = credit_doc.read()
             
         lines = content.split('\n')
         
         line_array = []
-        max_lines = 200
+        max_lines = 1000
         i = 0
         for line in lines:
             if len(line) > 0:
